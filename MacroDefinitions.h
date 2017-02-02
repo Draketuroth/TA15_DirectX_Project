7c214a5c69534cb144425137e823346b2dc3b5a5
Fredrik Linde <gearfreak@hotmail.se>
2017-02-02 12:49:19 +0100

    Deleted old version for transition to Extentions
    Got rid of old version in order to switch to GitHub extensions

--- MacroDefinitions.h
+++
@@ -1,7 +0,0 @@
-
-#define PI 3.14159265359
-
-#define WIDTH 1024
-#define HEIGHT 768
-
-#define LaunchInWindowedMode TRUE;
--- Render.h
+++
@@ -1,6 +0,0 @@
-
-#include "GraphicComponents.h"
-#include "BufferComponents.h"
-#include "TextureComponents.h"
-
-void Render(GraphicComponents &gHandler, BufferComponents &bHandler, TextureComponents &tHandler, FbxImport &fbxImporter, Terrain &terrain);
